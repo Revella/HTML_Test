ec13662a92ad7ed76e4a41fcc075ace0e8e76b1a
Revella <123723960+Revella@users.noreply.github.com>
2023-03-16 22:08:23 +0900

    Add files via upload
    change

--- 03.14/mobile web page/JS/script.js
+++ 03.14/mobile web page/JS/script.js
@@ -3,13 +3,14 @@ $(function() {
   $('.main-header-include').load('/include/main-header.html');
 
   /* Category Accodian */
-  $('.category-accordian .detail').eq(0).show()
+  $('.category-accordian .detail').eq(0).show();
   $('.category-accordian .title').click(function() {
     $(this).next().stop().slideDown();
-    $(this).siblings('.category-accordian .title').next().stop().slideup();
+    $(this).siblings('.category-accordian .title').next().stop().slideUp();
 
     $(this).addClass('active')
-    $(this).siblings('category-accordian .title').removeClass('active');
+    $(this).siblings('.category-accordian .title').removeClass('active');
+    // $(this).removeClass('active');
 
   })
 })
