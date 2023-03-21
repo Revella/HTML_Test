b6efecbb9abb29abd59b7dc7fec902f295245b81
Revella <123723960+Revella@users.noreply.github.com>
2023-03-21 17:25:33 +0900

    Add files via upload

--- 03.14/mobile web page/js/script.js
+++ 03.14/mobile web page/js/script.js
@@ -64,4 +64,23 @@ $(function() {
     $('.company-info-more').toggle()
   }); 
 
+  /* Goods Accordion */ 
+  $('.goods-accordion .detail').eq(0).show();
+  $('.goods-accordion .title').click(function() {
+    $(this).next().toggle();
+    $(this).addClass('active');
+    $(this).siblings('.goods-accordion .title').removeClass('active');
+  });
+
+  /* Goods Order Info */
+  $('.btn-order-choice').click(function() {
+    $('.goods-order-choice').hide();
+    $('.goods-order-final').slideDown(250);
+    $('.overlay-fold').show();
+  });
+  $('.btn-fold, .overlay-fold').click(function() {
+    $('.goods-order-choice').show();
+    $('.goods-order-final').slideUp(250);
+    $('.overlay-fold').hide();
+  });
 });
