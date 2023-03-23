77f71dd24fdeeedf913da53aec6c6b085773f797
Revella <123723960+Revella@users.noreply.github.com>
2023-03-23 12:47:20 +0900

    Add files via upload

--- 03.21/responsive-web/custom.js
+++ 03.21/responsive-web/custom.js
@@ -30,4 +30,16 @@ $(function() {
     slidesToShow: 4,
     slidesToScroll: 2
   });
+
+  /* Project-photo Slick */ 
+  $('.project-photo').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    cssEase: 'linear'
+  });
 });
