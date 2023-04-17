71204391745ad8f9a03654d83ae6ddbdd0b820a8
Revella <123723960+Revella@users.noreply.github.com>
2023-04-17 22:31:26 +0900

    change

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,7 +1,18 @@
+//Main
+const mainPic = document.querySelector(".pic");
+const pics = ["main-img_1.jpg", "main-img_2.jpg"]
+mainPic.style.backgroundImage = `url(/img/${pics[1]})`;
+
+// main gnb
+
+
+// about -content
 const aboutText = document.querySelector(".about-txt");
-const aboutPic = document.querySelector(".about-pic > img");
+const aboutPic = document.querySelector("img");
 
 aboutPic.addEventListener("mouseover", () => {
-  // aboutText.style.transform  = `translateX(-10%)`;
-  aboutText.style.scale = 1.2;
+  aboutText.style.scale = 1.15;
+})
+aboutPic.addEventListener("mouseout", () => {
+  aboutText.style.scale = 1;
 })
