5e51839587af1ab955b7b2e77910d7343f51f230
Revella <123723960+Revella@users.noreply.github.com>
2023-04-19 18:21:07 +0900

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,16 +1,30 @@
 /* Main */
 const mainPic = document.querySelector(".pic");
-const pics = ["main-img_1.jpg", "main-img_2.jpg"]
-mainPic.style.backgroundImage = `url(/img/${pics[1]})`;
+const pics = ["main-img_1.jpg", "main-img_2.jpg", "main-img_1.jpg", "main-img_2.jpg"]
+// mainPic.style.backgroundImage = `url(/img/${pics[1]})`;
 
+const arrows = document.querySelectorAll(".arrow");
+let i = 0;
+
+arrows.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    if(e.target.classList === "btn_back") {
+      i--;
+      if (i < 0) {
+        i = pics.length -1;
+      }
+    } else if (e.target.classList === "btn_next") {
+      i++;
+      if( i >= pics.length) {
+        i = 0;
+      }
+    }
+  mainPic.style.backgroundImage = `url(/img/${pics[i]})`;
+  })
+})
 // main gnb
-let mainNav = document.querySelectorAll(".gnb a");
+let mainNav = document.querySelectorAll(".navBtn");
 
-// mainNav.addEventListener("click", function() {
-//   mainNav.classList.add("focus");
-//   console.log(e);
-  
-// })
 
 // about -content
 const aboutContents = document.querySelector(".about-contents");
@@ -28,6 +42,7 @@ const DayNigth = document.querySelector(".dark-btn");
 
 DayNigth.addEventListener("click", () => {
   document.body.classList.toggle("on");
+
   aboutText.classList.toggle("on");
   if(DayNigth.innerHTML == "Dark") {
     DayNigth.innerHTML = "Day"
